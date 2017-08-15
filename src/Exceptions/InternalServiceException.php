95e27ed6b091deb1661ef00bdafa2a92c5469aa2
Carlos Eduardo <juniorb2ss@gmail.com>
2017-08-15 09:29:05 -0300

    Fixes PSR limit characters per line

--- src/Exceptions/InternalServiceException.php
+++ src/Exceptions/InternalServiceException.php
@@ -8,5 +8,5 @@ namespace juniorb2ss\DeathByCaptcha\Exceptions;
  */
 class InternalServiceException extends ClientException
 {
-    protected $message = 'Unable to complete request. Check your account credentials, balance, and ensure you\'re using the API correctly. If you\'re receiving this message on our API, please check in our website for Updates notices. If this problem persists, please contact support at help@deathbycaptcha.eu';
+    protected $message = 'Check your credentials, balance, and ensure you\'re using the API correctly.';
 }
